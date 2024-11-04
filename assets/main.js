82afdaf9011355035eef2022563dc9ad69018309
evelyn07072007 <160125244+evelyn07072007@users.noreply.github.com>
2024-11-04 11:46:06 -0300

    Add files via upload

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,20 @@
-function criaCartao(tema,pergunta,resposta)_{
-
-
-
+function criaCartao(tema,pergunta,resposta){
+    const container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartao.innerHTML = `
+    <div class="cartao-conteudo">
+     <h3> ${tema}</h3>
+    <div class="cartao-conteudo-pergunta">
+        ${pergunta}
+    </div>
+    <div class="cartao-conteudo-resposta">
+    ${resposta}
+    </div>
+    </div>
+    `
+    container.appendChild(cartao)
+    
 }
+criaCartao('Historia','Em que ano iniciou  a guerra do Vietnã?','Em 1955')
+criaCartao('portugues', 'o que e paraxiona?','uma paroxitona e uma palavracuja silaba tonica e a penultima.')
